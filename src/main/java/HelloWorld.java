39b0a5c3696d03cac21a39b371c46a4878636ae3
Evgenia <evgenia.pisko@regnology.net>
2025-09-16 08:32:01 +0000

    4st remote comment

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,5 @@ public static void main(String[] args) {
 // Prints "Hello, World" to the terminal window.
 System.out.println("Hello, World");
 System.out.println("Hello, World!");
-
+System.out.println(Hi, World!");
 }}
